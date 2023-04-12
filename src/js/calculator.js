be8c1b953c1ba0e053588d94e99eacbbaa2521e9
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-12 11:40:30 -0700

    Update branching logic in .checkLeapYear to pass the updated 2nd test.

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -8,8 +8,8 @@ export class Date {
   checkLeapYear() {
     if (this.year%400 === 0)  {
       return true;
-    // } else if ((this.year%4 === 0) && (this.year%100 !== 0))  {
-    //   return true;
+    } else if ((this.year%4 === 0) && (this.year%100 !== 0))  {
+      return true;
     } else {
       return false;
     }
