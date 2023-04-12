188193c6385003638514136113063458e3c2f4dc
Gareth Grindeland <gt.grindeland@gmail.com>
2023-04-12 11:39:09 -0700

    Update 2nd test to account for additional leap year rules.

--- __tests__/calculator.test.js
+++ __tests__/calculator.test.js
@@ -13,9 +13,13 @@ describe('Date', () => {
     expect(date.year).toEqual(2001);
   });
 
-  test('should return true if the date occurs on a leap year, starting only with years that are divisible by 400', () =>  {
+  test('should return true if the date occurs on a leap year', () =>  {
     let dateInLeapYear = new Date (1,1,2000); //this leap year is divisible by 400
+    let dateInLeapYear2 = new Date (1,1,2004); //divisible by 4 but not by 100
+    let dateNotInLeapYear = new Date (1,1,1900); //divisible by 4 and by 100, so not a leap year
     expect(dateInLeapYear.checkLeapYear()).toEqual(true);
     expect(date.checkLeapYear()).toEqual(false);
-  })
+    expect(dateInLeapYear2.checkLeapYear()).toEqual(true);
+    expect(dateNotInLeapYear.checkLeapYear()).toEqual(false);
+  });
 });
--- src/js/calculator.js
+++ src/js/calculator.js
@@ -8,9 +8,11 @@ export class Date {
   checkLeapYear() {
     if (this.year%400 === 0)  {
       return true;
+    // } else if ((this.year%4 === 0) && (this.year%100 !== 0))  {
+    //   return true;
     } else {
       return false;
     }
   }
-  
+
 }
